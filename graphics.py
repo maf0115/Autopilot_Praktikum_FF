a56a7bfb201df5b97ab86aaaaf062d4a195591ed
maf0115 <maf0115@thi.de>
2024-06-06 00:56:13 +0200

    Figured out coordinate conversion for X-axis

--- graphics.py
+++ graphics.py
@@ -4,9 +4,10 @@ from pygame.image import load
 from pygame.transform import scale
 from os import path
 import time
+import random
+from math import cos
 
 # Images
-FIGHTER_JET = load(path.join(".\Images", "FighterJetIcon.png"))
 MANCHING_MAP = load(path.join(".\Images", "Manching.png"))
 WINDOW_LOGO = load(path.join(".\Images", "WindowLogo.png"))
 
@@ -14,43 +15,42 @@ WINDOW_LOGO = load(path.join(".\Images", "WindowLogo.png"))
 CANVAS_X = MANCHING_MAP.get_width()
 CANVAS_Y = MANCHING_MAP.get_height()
 
+
 # Color for the trace to keep track of what pattern the aricraft flew
 TRACE_COLOR = (221, 25, 224)
 BLACK = (0, 0, 0)
+FIGTHER_JET_COLOR = (150, 255, 100)
+
+# Map some coordinates to pixel values to get some reference points
+MIN_X = (29/60, CANVAS_X/2 - 3 * CANVAS_X/26 - 2)
+MAX_X = (35/60, CANVAS_X/2 + 3 * CANVAS_X/26 + 2)
+MIN_Y = (40/60, CANVAS_Y/2 + 6 * CANVAS_Y/17 + 2)
+MAX_Y = (50/60, CANVAS_Y/2 - 6 * CANVAS_Y/17 - 2)
 
 # Main window and game loop
 SCREEN = display.set_mode((CANVAS_X, CANVAS_Y), RESIZABLE)
+RUNNING = True
 
-# Reference points for coordinate calculations
-MINS_X = 25.0
-MINS_Y = 12.0
-
-def setup_window_info()->None:
-    global RUNNING   
+def setup_window_info()->None:   
     display.set_icon(WINDOW_LOGO)
     display.set_caption("Navigation System GUI (maf0115)") 
 
+def generate_dummy_coordinates(): 
+    return [random.uniform(11 + 20/60, 11 + 45/60), 
+            random.uniform(48 + 36/60, 48 + 52/50),
+            random.randint(0, 360)]
 
 def get_coordinates(lat : float, lon : float)->tuple: 
-    """
-    This function converts the given coordinates into pixels that will be displayed on the image. 
+    
+    # 0 is for coordinates
+    # 1 is for pixels 
+    x = (((lat % 1) - MAX_X[0]) * (MAX_X[1] - MIN_X[1])) / (MAX_X[0] - MIN_X[0]) + MAX_X[1]
 
-    Args: 
-        lat: the given latitude
-        lion: the given longitude
-    Returns: 
-        Tuple (x, y)
-    """
-    # lat starts from 11° 20' to 11° 45' 
-    x = ((lat - 11) * 60 * CANVAS_X) / MINS_X
-    y = ((lon - 48) * 60 * CANVAS_Y) / MINS_Y
+    y = (((lon % 1) - MAX_Y[0]) * (MAX_Y[1] - MIN_Y[1])) / (MAX_Y[0] - MIN_Y[0]) + MAX_Y[1]
 
-    print(f'CANVAS_X : {CANVAS_X}\tCANVAS_Y: {CANVAS_Y}')
-    print(f'x : {x}\ty:{y}')
-
+    print(f'x: {x}\ty: {y}')
     return (x, y)
 
-    # lon starts from 48° 52' to 48° 36'
 
 
 def draw_fighter_jet(lat : float, lon : float, true_heading : float)->None: 
@@ -63,9 +63,10 @@ def draw_fighter_jet(lat : float, lon : float, true_heading : float)->None:
         true_heading: the angle at which the image is to be drawn
 
     Returns: 
-        tuple containing x for lat and y for lon
+        None
     """
-
+    coords = get_coordinates(lat, lon)
+    fighter_jet = pygame.draw.polygon(SCREEN, [])
 
 
 def dummy_main_loop()->None:
@@ -79,32 +80,29 @@ def dummy_main_loop()->None:
     """
     pygame.init()
     setup_window_info()
-    RUNNING = True 
+    RUNNING = True
 
     while RUNNING: 
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: 
                 RUNNING = False 
+
+            # Events with keys
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    RUNNING = False
         
         # Get the coordinates for the current point
-        coords = get_coordinates(11.5, 48.71666)
+        # coords = generate_dummy_coordinates()
+        converted_coords = get_coordinates(11.666667, 48.666667)
 
         # Display the reference image
         SCREEN.blit(scale(MANCHING_MAP, (CANVAS_X, CANVAS_Y)), (0, 0))
-        pygame.draw.line(SCREEN, 
-                         TRACE_COLOR, 
-                         (CANVAS_X/2, 0),
-                         (CANVAS_X/2,CANVAS_Y), 5)
         
-        pygame.draw.line(SCREEN, 
-                         TRACE_COLOR, 
-                         (0, CANVAS_Y/2),
-                         (CANVAS_X,CANVAS_Y/2), 5)
-        
-        pygame.draw.circle(SCREEN, BLACK, coords, 5.0)
+        pygame.draw.circle(SCREEN, BLACK, converted_coords, 5.0)
         
         display.update()
-        time.sleep(10)
+        time.sleep(1)
 
 if __name__ == '__main__': 
     dummy_main_loop()
