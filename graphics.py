c44e79b94cc41a5dd57a5f2b450d2c0ba3d80669
maf0115 <maf0115@thi.de>
2024-06-06 15:41:25 +0200

    Coordinate mapping integrated

--- graphics.py
+++ graphics.py
@@ -3,9 +3,7 @@ from pygame import display, RESIZABLE
 from pygame.image import load
 from pygame.transform import scale
 from os import path
-import time
 import random
-from math import cos
 
 # Images
 MANCHING_MAP = load(path.join(".\Images", "Manching.png"))
@@ -22,14 +20,20 @@ BLACK = (0, 0, 0)
 FIGTHER_JET_COLOR = (150, 255, 100)
 
 # Map some coordinates to pixel values to get some reference points
-MIN_X = (29/60, CANVAS_X/2 - 3 * CANVAS_X/26 - 2)
-MAX_X = (35/60, CANVAS_X/2 + 3 * CANVAS_X/26 + 2)
-MIN_Y = (40/60, CANVAS_Y/2 + 6 * CANVAS_Y/17 + 2)
-MAX_Y = (50/60, CANVAS_Y/2 - 6 * CANVAS_Y/17 - 2)
+# These coordinates were obtained by clicking on some specific coordinates on the image and getting the pixel values of the mouse cursor
+MIN_X = (29/60, 250)
+MAX_X = (35/60, 405)
+MIN_Y = (50/60, 90) 
+MAX_Y = (40/60,  480)
 
 # Main window and game loop
 SCREEN = display.set_mode((CANVAS_X, CANVAS_Y), RESIZABLE)
 RUNNING = True
+
+# List to keep track of the jet's trail and all of the stuff it needs as well 
+TRAIL_LIST = list()
+TRAIL_MAX_LEN = 5
+TRAIL_POINT_DISTANCE = 100
 
 def setup_window_info()->None:   
     display.set_icon(WINDOW_LOGO)
@@ -46,7 +50,8 @@ def get_coordinates(lat : float, lon : float)->tuple:
     # 1 is for pixels 
     x = (((lat % 1) - MAX_X[0]) * (MAX_X[1] - MIN_X[1])) / (MAX_X[0] - MIN_X[0]) + MAX_X[1]
 
-    y = (((lon % 1) - MAX_Y[0]) * (MAX_Y[1] - MIN_Y[1])) / (MAX_Y[0] - MIN_Y[0]) + MAX_Y[1]
+    # The problem with this coordinate is that the y-axis is not divided up uniformingly -> Get another inerval, both in coordinates and in pixel values
+    y = (((lon % 1) - MAX_Y[0]) * (MAX_Y[1] - MIN_Y[1])) / (MAX_Y[0] - MIN_Y[0]) + MAX_Y[1] 
 
     print(f'x: {x}\ty: {y}')
     return (x, y)
@@ -94,15 +99,27 @@ def dummy_main_loop()->None:
         
         # Get the coordinates for the current point
         # coords = generate_dummy_coordinates()
-        converted_coords = get_coordinates(11.666667, 48.666667)
+        converted_coords = get_coordinates(11.58333333, 48.66666667)
+
 
         # Display the reference image
         SCREEN.blit(scale(MANCHING_MAP, (CANVAS_X, CANVAS_Y)), (0, 0))
-        
+
+        # Display some reference lines to get good points for the x coordinate
+        pygame.draw.line(SCREEN, 
+                         TRACE_COLOR, 
+                         (0, CANVAS_Y/2 - CANVAS_Y/24.5), 
+                         (CANVAS_X, CANVAS_Y/2 - CANVAS_Y/24.5), 
+                         2)
+
+        # Draw point for reference
         pygame.draw.circle(SCREEN, BLACK, converted_coords, 5.0)
-        
+
+        if pygame.mouse.get_pressed(num_buttons=3)[0]:
+            mouse_pos = pygame.mouse.get_pos()
+            print(f'mouse_x : {mouse_pos[0]}\tmouse_y: {mouse_pos[1]}')
         display.update()
-        time.sleep(1)
+
 
 if __name__ == '__main__': 
     dummy_main_loop()
