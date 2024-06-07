3c0325faf33452c50cc7fb113c3f741d90b33e9d
maf0115 <maf0115@thi.de>
2024-06-07 13:24:41 +0200

    added ils interface

--- graphics.py
+++ graphics.py
@@ -1,22 +1,35 @@
 import pygame
+from pygame.draw import circle, rect
+from pygame import font, Rect
 from pygame import display, RESIZABLE 
 from pygame.image import load
-from pygame.transform import scale
-from pygame.math import Vector2
 from os import path
 
 # Images
-MANCHING_MAP = load(path.join(".\Images", "Manching.png"))
-WINDOW_LOGO = load(path.join(".\Images", "WindowLogo.png"))
+WINDOW_LOGO = load(path.join(".\Images", "ILS_Icon.png"))
 
 # Screen Measurements
-CANVAS_X = MANCHING_MAP.get_width()
-CANVAS_Y = MANCHING_MAP.get_height()
+CANVAS_SIZE = 800
+CIRCLE_RADIUS = CANVAS_SIZE/2 - 50
+CENTER = (CANVAS_SIZE/2, CANVAS_SIZE/2)
 
-# Color for the trace to keep track of what pattern the aricraft flew
-TRACE_COLOR = (221, 25, 224)
+# Reference points macros
+REF_POINT_AMT = 6
+REF_POINT_DIST = CIRCLE_RADIUS/REF_POINT_AMT
+REF_POINT_RADIUS = 10
+
+# INOP flag measurements
+INOP_SIDE_X = 150
+INOP_SIDE_Y = 100
+INOP_COORD_X = CANVAS_SIZE/2 + 2*REF_POINT_DIST
+INOP_COORD_Y = CANVAS_SIZE/2 - 3*REF_POINT_DIST
+INOP_DIST = 20
+
+# Colors
 BLACK = (0, 0, 0)
-FIGTHER_JET_COLOR = (150, 255, 100)
+SLOPE_INDICATOR_COLOR = (150, 255, 100)
+ILS_BORDER_COLOR = (227, 225, 218)
+INOP_FLAG_COLOR = (255, 0, 0)
 
 # Map some coordinates to pixel values to get some reference points
 # These coordinates were obtained by clicking on some specific coordinates on the image and getting the pixel values of the mouse cursor
@@ -26,110 +39,75 @@ MIN_Y = (50/60, 90)
 MAX_Y = (40/60, 480)
 
 # Main window and game loop
-SCREEN = display.set_mode((CANVAS_X, CANVAS_Y), RESIZABLE)
+SCREEN = display.set_mode((CANVAS_SIZE, CANVAS_SIZE), RESIZABLE)
 RUNNING = True
 
-# List to keep track of the jet's trail and all of the stuff it needs as well 
-TRAIL_LIST = list()
-TRAIL_MAX_LEN = 5
-TRAIL_POINT_DISTANCE = 100
-TRAIL_POINT_CNT = 0
-
-# Variables to draw the fighter jet
-Y_DIST_COW = 13
-BASE = 6.5
-
-def setup_window_info()->None:   
+def setup_window_info()->None: 
+    pygame.init()
     display.set_icon(WINDOW_LOGO)
-    display.set_caption("Navigation System GUI (maf0115)") 
+    display.set_caption("Instrument Landing System (maf0115)")
 
-
-def get_coordinates(sim_data)->tuple:    
-    # 0 is for coordinates
-    # 1 is for pixels 
-    lat = sim_data[1]
-    lon = sim_data[0]
-    return(
-        (((lat % 1) - MAX_X[0]) * (MAX_X[1] - MIN_X[1])) / (MAX_X[0] - MIN_X[0]) + MAX_X[1],
-        (((lon % 1) - MAX_Y[0]) * (MAX_Y[1] - MIN_Y[1])) / (MAX_Y[0] - MIN_Y[0]) + MAX_Y[1]
-        )
-
-def draw_fighter_jet(lat : float, lon : float, true_heading : float)->None: 
-    """
-    This function draws the fighter jet icon at a determined angle and coordinates 
-
-    Args: 
-        lat: the latitude (x-coordinate over Manching image)
-        lon: the longitude (y-coordinate over Manching image)
-        true_heading: the angle at which the image is to be drawn
-
-    Returns: 
-        None
-    """
-
-    triangle_filling = [(lat, lon - Y_DIST_COW), 
-                        (lat - BASE , lon +  Y_DIST_COW), 
-                        (lat + BASE , lon +  Y_DIST_COW)]
+def draw_ils()->None:
+    SCREEN.fill(BLACK)
+    circle(SCREEN, 
+           ILS_BORDER_COLOR, 
+           CENTER, 
+           CIRCLE_RADIUS, 
+           5)
     
-    pp = Vector2((lat, lon))
-    rotated_triangle = [
-        (pygame.math.Vector2(x, y) - pp).rotate(true_heading) + pp for x, y in triangle_filling]
-                          
-    pygame.draw.polygon(SCREEN, 
-                        TRACE_COLOR,
-                        rotated_triangle, 
-                        0) 
+    def draw_ref_points(start : tuple, vert_dir : int, hor_dir : int )->None:
+        x_start = start[0]
+        y_start = start[1]
 
-def draw_trail(lat : float, lon : float)->None:
-    """
-    This function ups a counter to keep track of how many points are rendered. for every TAIL_POINT_DISTANCE points, it saves the coodinates of said point in the list and displays it. The point gets then eliminated once the list adds another point and it reached TRAIL_MAX_LEN 
+        cnt = 0
 
-    Args: 
-
-    Returns: 
-        None
-    """ 
-    global TRAIL_POINT_CNT
-    global TRAIL_POINT_DISTANCE
-    global TRAIL_LIST
-    global TRAIL_MAX_LEN
-
-    if TRAIL_POINT_CNT >= TRAIL_POINT_DISTANCE: 
-        if len(TRAIL_LIST) == TRAIL_MAX_LEN: 
-            TRAIL_LIST.pop(0)
-        TRAIL_LIST.append((lat, lon))
-        TRAIL_POINT_CNT = 0
-    else: 
-        TRAIL_POINT_CNT += 1
-
-    # Render the points
-    for point in TRAIL_LIST: 
-        pygame.draw.circle(SCREEN,
-                           TRACE_COLOR, 
-                           point, 
-                           5)
+        while cnt < REF_POINT_AMT:
+            circle(SCREEN,
+                    ILS_BORDER_COLOR, 
+                    (x_start, y_start), 
+                    REF_POINT_RADIUS)
 
 
-def draw_scene(sim_data : list)->None: 
-    """
-    This function draws the scene, including the main map, the fighter jet and the list of points
+            # Update coordinates
+            cnt += 1
+            x_start = CANVAS_SIZE/2 + abs(cnt * REF_POINT_DIST) * hor_dir
+            y_start = CANVAS_SIZE/2 + abs(cnt * REF_POINT_DIST) * vert_dir
+            
+        
+    draw_ref_points(CENTER, 1, 0)
+    draw_ref_points(CENTER, -1, 0)
+    draw_ref_points(CENTER, 0, 1)
+    draw_ref_points(CENTER, 0, -1)
 
-    Args: 
-        sim_data: the simulation data 
-    """
+def draw_inop_flag()->None: 
+    inop_rect = Rect((INOP_COORD_X + INOP_DIST, 
+                      INOP_COORD_Y + INOP_DIST, 
+                      INOP_SIDE_X, 
+                      INOP_SIDE_Y))
+    
+    text_surface_object = font.SysFont('Arial', 50).render('INOP', True, BLACK)
 
-    # Display the reference image
-    SCREEN.blit(scale(MANCHING_MAP, (CANVAS_X, CANVAS_Y)), (0, 0))
+    text_rect = text_surface_object.get_rect(center = inop_rect.center)
+    rect(SCREEN, INOP_FLAG_COLOR, inop_rect)
+    SCREEN.blit(text_surface_object, text_rect)
 
-    # Convert the coordinates of the jet for every function that needs them
-    lat, lon = get_coordinates(sim_data)
+def test_main():
+    setup_window_info()
+    global RUNNING
+    while RUNNING:
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                RUNNING = False 
 
-    # Draw fighter jet icon
-    draw_fighter_jet(lat, lon, sim_data[5])
+            # Events with keys
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    RUNNING = False
 
-    # Render the trail
-    draw_trail(lat, lon)
+        draw_ils()
+        draw_inop_flag()
+        display.update()
 
-    # Update the screen info
-    display.update()
+if __name__ == '__main__': 
+    test_main()
   
