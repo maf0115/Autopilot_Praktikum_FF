70b077ae6eea3f0247193fb69727b6dfd001037b
maf0115 <maf0115@thi.de>
2024-06-21 17:33:16 +0200

    Fixed time and distance, time has weid bug while tracing wypts to the right

--- wyp.py
+++ wyp.py
@@ -1,7 +1,7 @@
 # Put the waypoints in a class
 from pygame import Rect
 import moving_map_conf as mmc
-from math import sin, asin, cos, acos, atan2, degrees, pow, sqrt, pi
+from math import sin, asin, cos, radians, atan2, degrees, pow, sqrt, pi
 
 class Waypoint: 
     def __init__(self, x, y): 
@@ -37,7 +37,7 @@ class Connection:
         mmc.WYP_CONNECTION_CNT += 1
 
         # Data regarding assignment 3 
-        self.tas = 100.0 * 1852.0 / 3600.0
+        self.gs = 100.0 * 1852.0 / 3600.0
         self.time = float
         self.verbose_time = str
         self.rwk = float
@@ -52,9 +52,9 @@ class Connection:
         Returns: 
             distance in float
         """
-        hypothenuse = sqrt(pow(self.start.get_lon() - self.finish.get_lon(), 2) + pow(self.start.get_lat() - self.finish.get_lat(), 2))
-
-        self.distance = hypothenuse * 60
+        self.distance = sqrt(pow((self.start.get_lat() - self.finish.get_lat()) * 60.0, 2) + \
+                             pow((self.start.get_lon()-self.finish.get_lon())*cos(radians(self.start.get_lat())) * 60.0, 2))
+        return self.distance
 
 
     def get_rwk(self)->None: 
@@ -90,6 +90,7 @@ class Connection:
         rads = atan2(-dy, dx)
         rads %= 2*pi
         self.rwk = 360 - degrees(rads)
+        return self.rwk
      
 
 
@@ -97,23 +98,19 @@ class Connection:
         return mmc.WIND_DIRECTION - self.rwk * pi/180
     
     def get_luv(self)->float: 
-        return asin(mmc.WIND_SPEED * sin(self.get_beta()/self.tas))
+        return asin(mmc.WIND_SPEED * sin(self.get_beta()/self.gs))
     
     def get_gamma(self)->float:
         return pi - (self.get_luv() + self.get_beta())
     
     def get_ground_speed(self)->float:
-        return sqrt(pow(self.tas, 2) + pow(mmc.WIND_SPEED, 2)) - 2 * mmc.WIND_SPEED * self.tas * cos(self.get_gamma())
+        return sqrt(pow(self.gs, 2) + pow(mmc.WIND_SPEED, 2)) - 2 * mmc.WIND_SPEED * self.gs * cos(self.get_gamma())
     
     def get_est_time(self)->float:
-        self.time = abs(self.distance)/abs(self.get_ground_speed()) 
+        self.time = abs(self.distance * 1852)/abs(self.get_ground_speed()) 
 
     def convert_time_to_verbose(self)->str:
-        hours = self.time // 1
-        minutes = ((self.time % 1) * 60/100) // 1
-        seconds = ((self.time % 1) * 3600/100) // 1
-
-        self.verbose_time = f'{hours}h {minutes}min {seconds}s'
+        self.verbose_time = '{0:02.0f}:{1:02.0f}'.format(*divmod(self.time, 60))
 
     def set_connection_info(self): 
         # Set all the data
@@ -125,6 +122,6 @@ class Connection:
         self.data = f'Start: WP{self.start.index}; End: WP{self.finish.index}{mmc.TXT_TAB}'
         self.data += f'Distance: {str(self.distance)[:7]}NM{mmc.TXT_TAB}'
         self.data += f'RWK: {str(self.rwk)[:7]}°{mmc.TXT_TAB}'
-        self.data += f'ETA: {str(self.time)[:7]}h'
+        self.data += f'ETA: {self.verbose_time}'
 
         
